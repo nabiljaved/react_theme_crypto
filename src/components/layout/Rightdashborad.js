1019d4f5af8b3576e90afea5aeadc5c324a2a3df
nabiljaved <nabeeljaved2016@gmail.com>
2020-10-03 14:32:39 -0700

    added graph section

--- src/components/layout/Rightdashborad.js
+++ src/components/layout/Rightdashborad.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../sections/topsection/Header";
+import Graph from "../../sections/topsection/Graph";
 
 const Rightdashborad = (props) => {
   return (
     <div className="Right-dashboard">
       <Header />
+      <Graph />
     </div>
   );
 };
---
+++ src/sections/topsection/Graph.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Graph = (props) => {
+  return (
+    <div className="section-graph">
+      <div className="section-gaph-top">
+        <div className="graph-section-circle-icons ">
+          <i className="fas fa-user" style={{ color: "#444061" }}></i>
+        </div>
+
+        <div className="graph-section-list-dates">
+          <ul className="graph-section-list-dates-items">
+            <li>1h</li>
+            <li>3h</li>
+            <li style={{ background: "grey", borderRadius: "30px" }}>1d</li>
+            <li>7d</li>
+            <li>1m</li>
+            <li>All</li>
+          </ul>
+        </div>
+
+        <div className="graph-section-forex-index">
+          <ul className="graph-section-forex-index-items">
+            <li>open : 2456.45</li>
+            <li>Height : 3456</li>
+            <li>Low : 18765</li>
+            <li>Close : 14567.0</li>
+          </ul>
+        </div>
+
+        <div className="graph-section-circle-icons hide-graph-icons">
+          <i className="fas fa-arrow-left" style={{ color: "#444061" }}></i>
+        </div>
+
+        <div className="graph-section-circle-icons ml-2 hide-graph-icons">
+          <i className="fas fa-arrow-right" style={{ color: "#444061" }}></i>
+        </div>
+
+        <div className="graph-section-circle-icons ml-2 hide-graph-icons">
+          <i className="fas fa-plus" style={{ color: "#444061" }}></i>
+        </div>
+      </div>
+      <hr class="solid" />
+      <div className="section-gaph-middle">
+        <div className="section-gaph-middle-graph-image"></div>
+      </div>
+      <div className="section-gaph-bottom"></div>
+    </div>
+  );
+};
+
+export default Graph;
